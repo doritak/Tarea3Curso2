e0c3090953befdde54c5477553dacf6e8774ae0c
doritak <104630627+doritak@users.noreply.github.com>
2024-10-20 22:59:08 -0300

    Min y max por años de experiencia intervalos 5años

--- consultas.py
+++ consultas.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def cargar_dataset(ruta_dataset: str):
@@ -63,7 +64,52 @@ def calcular_ingresos_por_pais(dataframe):
     return df_ordenado_paises
 
 def calcular_ingresos_por_experiencia(dataframe):
-    return dataframe[dataframe['Country']=='Chile']
+    df = dataframe.loc[:, ["YearsCode","YearsCodePro", "ConvertedCompYearly"]]  # realizo un dataframe con tres columnas 
+    # df["Age"] = df["Age"].str.split(" ",expand=True)[0]    
+    df["YearsCode"] = np.where(df["YearsCode"]=="Sin información", "0", df["YearsCode"])
+    df["YearsCode"] = np.where(df["YearsCode"]=="Less than 1 year", "0", df["YearsCode"])
+    df["YearsCode"] = np.where(df["YearsCode"]=="More than 50 years", "51", df["YearsCode"])
+
+    df["YearsCodePro"] = np.where(df["YearsCodePro"]=="Sin información", "0", df["YearsCodePro"])
+    df["YearsCodePro"] = np.where(df["YearsCodePro"]=="Less than 1 year", "0", df["YearsCodePro"])
+    df["YearsCodePro"] = np.where(df["YearsCodePro"]=="More than 50 years", "51", df["YearsCodePro"])
+    
+    df.drop(df[df["ConvertedCompYearly"]=="Sin información"].index, inplace=True)
+    
+    df["YearsCode"] = df["YearsCode"].astype('float64')
+    df["YearsCodePro"] = df["YearsCodePro"].astype('float64')
+    df["ConvertedCompYearly"] = df["ConvertedCompYearly"].astype('float64')
+    
+    df.drop(df[df["ConvertedCompYearly"]=="Sin información"].index, inplace=True)
+    
+    df["Años de experiencia"] = df["YearsCode"] + df["YearsCodePro"]
+    
+    df.drop(df[df["Años de experiencia"]>55].index, inplace=True)
+    lista =[]
+    rango = [0,5,10,15,20,25,30,35,40,45,50,55]
+    for i in range(len(rango)-1):
+        df_intervalo_min = df.loc[(df["Años de experiencia"]>=rango[i]) & (df["Años de experiencia"]<rango[i+1])].groupby("Años de experiencia").min()
+        df_intervalo_max = df.loc[(df["Años de experiencia"]>=rango[i]) & (df["Años de experiencia"]<rango[i+1])].groupby("Años de experiencia").max()
+        lista.append([(str(rango[i])+"-"+str(rango[i+1])), df_intervalo_min["ConvertedCompYearly"].min(), df_intervalo_max["ConvertedCompYearly"].max()])
+    
+    df_intervalo = pd.DataFrame(lista, columns=["Años de experiencia","Intervalo inferior (USD/Año)","Intervalo superior (USD/Año)"])
+    
+    df_intervalo["Intervalo inferior (USD/Año)"] = df_intervalo["Intervalo inferior (USD/Año)"].map("{:,.0f}".format)
+    df_intervalo["Intervalo superior (USD/Año)"] = df_intervalo["Intervalo superior (USD/Año)"].map("{:,.0f}".format)
+    
+    # //////////////////////////////////////////
+    #creo un nuevo data frame con el indice de Años de experienciencia y me los agrupa con el valor mínimo y valor máximo
+    # df2 = df.pivot_table(index="Age", values="ConvertedCompYearly", aggfunc={np.min,np.max})
+    # #reseteo el indice para que los años de experiencia me queden como una columna mas
+    # df2.reset_index(inplace=True)
+    # #doy formato a los resultadso de max y min
+    # df2["max"] = df2["max"].map("{:,.0f}".format)
+    # df2["min"] = df2["min"].map("{:,.0f}".format)
+    # df.rename(columns={"Age":"Años de experiencia"}, inplace=True)
+    # df2.rename(columns={"max":"Intervalo superior (USD/Año)","min":"Intervalo inferior (USD/Año)"}, inplace=True)
+
+    print(df_intervalo)
+    return df_intervalo
 
 def calcular_empleabilidad(dataframe):
     return dataframe[dataframe['Country']=='Chile']
@@ -76,7 +122,10 @@ def graficar_ingresos_paises(dataframe):
 if __name__ == "__main__":
     df = cargar_dataset("developers_info.csv")
     df_limpios = limpiar_dataset(df,50)
-    df_p = calcular_ingresos_por_pais(df_limpios) 
-    graficar_ingresos_paises(df_p)
+    # df_p = calcular_ingresos_por_pais(df_limpios)
+    df_i = calcular_ingresos_por_experiencia(df_limpios)
+    print(df_i.dtypes) 
+
+    # graficar_ingresos_paises(df_p)
 
     
